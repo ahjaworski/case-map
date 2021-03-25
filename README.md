62f4531356eb30ea1fca0a565c6e1ba9597493bb
Antek Jaworski <ahjaworski@gmail.com>
2021-03-25 21:13:34 +0100

    add map and change title

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CaseRoadObjects
+# Case with Mapbox implementation
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.5.
 
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import mapboxgl, { Map as MapboxMap } from 'mapbox-gl';
 
 @Component({
@@ -6,15 +6,19 @@ import mapboxgl, { Map as MapboxMap } from 'mapbox-gl';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('mapContainer')
+  private mapContainer: ElementRef | undefined;
   private map: MapboxMap | null = null;
 
   public ngOnInit(): void {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYWhqYXdvcnNraSIsImEiOiJjazk4ZWtxamwwMXJ3M21wNHlnYTgxbWZrIn0.YmOmIQjlp2c6SE9eWtHtqA';
+  }
 
+  public ngAfterViewInit(): void {
     this.map = new MapboxMap({
-      container: 'map',
+      container: this.mapContainer?.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [5.127684, 52.0809856],
       zoom: 10,
